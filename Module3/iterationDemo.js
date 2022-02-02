b33a0fbfb8465ad0f7f04a251c7c8ec6f7fcb211
ThummalacharlaUsha <50477075+ThummalacharlaUsha@users.noreply.github.com>
2022-02-02 12:59:58 +0530

    Add files via upload

---
+++ Module3/iterationDemo.js
@@ -0,0 +1,37 @@
+import { LightningElement } from 'lwc';
+
+export default class IterationDemo extends LightningElement {
+
+    contacts=[
+        {
+            Id: 1,
+            Name: 'Test 1',
+            Email: 'test1@gmail.com',
+        },
+        {
+            Id: 2,
+            Name: 'Test 2',
+            Email: 'test2@gmail.com',
+        },
+        {
+            Id: 3,
+            Name: 'Test 3',
+            Email: 'test3@gmail.com',
+        },
+        {
+            Id: 4,
+            Name: 'Test 4',
+            Email: 'test4@gmail.com',
+        },
+        {
+            Id: 5,
+            Name: 'Test 5',
+            Email: 'test5@gmail.com',
+        },
+        {
+            Id: 6,
+            Name: 'Test 6',
+            Email: 'test6@gmail.com',
+        },
+    ];
+}
